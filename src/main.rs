f335487b04a9caf5d8fcc65ff3ab4fef2a459dbc
nikolaizombie1 <fabio.matos999@gmail.com>
2022-09-25 10:43:50 -0400

    Intial Commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::process::{Command, exit};
+fn main() {
+    let default_str = Command::new("bash").
+        arg("-c").
+        arg("pactl info | awk '/Default Sink: /{print $3}'").
+        output().
+        expect("Shell process failed").stdout;
+
+    let sink_string = String::from(
+        String::from_utf8_lossy(&default_str)
+    );
+    let mut name = String::new();
+    if sink_string.contains("Schiit") {name = "Schiit Modi 3+".to_string();}
+    else if sink_string.contains("hdmi") {name = "Monitor".to_string();}
+    else if sink_string.contains("pci") {name = "Built-in Audio".to_string();}
+    else if sink_string.contains("bluez") {name = "Bluetooth".to_string();}
+    println!("|{}|",name);
+    exit(0)
+}
